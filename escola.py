61a2f237a680246155c512047f6bafaaeab7ee6f
SilencioPz <silencioso_br@outlook.com>
2024-06-08 10:27:10 -0400

    Dados enviados para o projeto

--- escola.py
+++ escola.py
@@ -1,32 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
-class Aluno:
-    def __init__(self, nome, telefone, email):
-        self.nome = nome
-        self.telefone = telefone
-        self.uf = email
-aluno1 = Aluno('Fabiano','66999537642','fabianotaguchi@gmail.com')
-lista = [aluno1]
-
+#Criação da aplicação baseada no Flask
 app = Flask(__name__)
+#Realiza a conexão com o banco de dados
+app.config.from_pyfile('configuracao.py')
+csrf = CSRFProtect(app)
 
-@app.route('/')
-def index():
-    return render_template('lista.html', titulo='Listagem de alunos', alunos=lista)
+db = SQLAlchemy(app)
+from rotas import *
 
-@app.route('/novo')
-def novo():
-    return render_template('novo.html', titulo='Cadastro de aluno')
-
-@app.route('/criar', methods=['POST',])
-def criar():
-    nome = request. form['nome']
-    email = request. form['email']
-    telefone = request. form['telefone']
-    aluno = Aluno(nome, telefone, email)
-    lista.append(aluno)
-    return redirect(url_for('index'))
-
-app.run(debug=True)
+#Execução da aplicação
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
